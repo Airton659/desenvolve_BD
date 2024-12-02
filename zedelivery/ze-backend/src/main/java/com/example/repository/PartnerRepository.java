e37bb64be5a202fa508b69bad6833732f8d8732b
Airton659 <108145077+Airton659@users.noreply.github.com>
2024-12-02 23:36:27 +0000

    Projeto excluido

--- zedelivery/ze-backend/src/main/java/com/example/repository/PartnerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Partner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartnerRepository extends JpaRepository<Partner, String> {
-    
-}
